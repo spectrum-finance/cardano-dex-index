e7a7ab73c19d89eefd4d95910187940879d29c99
t.gusev <gusev.t.s1@gmail.com>
2022-06-11 22:48:29 +0300

    db writer errors handling added

--- modules/db-writer/src/main/scala/fi/spectrumlabs/db/writer/App.scala
+++ modules/db-writer/src/main/scala/fi/spectrumlabs/db/writer/App.scala
@@ -1,6 +1,5 @@
 package fi.spectrumlabs.db.writer
 
-import cats.data.NonEmptyList
 import cats.effect.{Blocker, Resource}
 import fi.spectrumlabs.core.EnvApp
 import fi.spectrumlabs.core.models.Tx
@@ -8,14 +7,11 @@ import fi.spectrumlabs.core.streaming.Consumer.Aux
 import fi.spectrumlabs.core.streaming.config.{ConsumerConfig, KafkaConfig}
 import fi.spectrumlabs.core.streaming.serde._
 import fi.spectrumlabs.core.streaming.{Consumer, MakeKafkaConsumer}
-import fi.spectrumlabs.db.writer.classes.Handle
+import fi.spectrumlabs.db.writer.Handlers._
 import fi.spectrumlabs.db.writer.config._
-import fi.spectrumlabs.db.writer.models._
-import fi.spectrumlabs.db.writer.models.db.{ExecutedDeposit, ExecutedRedeem, ExecutedSwap}
 import fi.spectrumlabs.db.writer.models.streaming.ExecutedOrderEvent
 import fi.spectrumlabs.db.writer.persistence.PersistBundle
-import fi.spectrumlabs.db.writer.programs.{Handler, HandlersBundle, WriterProgram}
-import fs2.Chunk
+import fi.spectrumlabs.db.writer.programs.{HandlersBundle, WriterProgram}
 import fs2.kafka.RecordDeserializer
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import org.apache.kafka.common.TopicPartition
@@ -39,11 +35,11 @@ object App extends EnvApp[AppContext] {
       configs <- Resource.eval(ConfigBundle.load[InitF](configPathOpt, blocker))
       ctx                                = AppContext.init(configs)
       implicit0(ul: Unlift[RunF, InitF]) = Unlift.byIso(IsoK.byFunK(wr.runContextK(ctx))(wr.liftF))
-      trans <- PostgresTransactor.make[InitF]("meta-db-pool", configs.pg)
+      trans <- PostgresTransactor.make[InitF]("db-writer-pool", configs.pg)
       implicit0(xa: Txr.Continuational[RunF]) = Txr.continuational[RunF](trans.mapK(wr.liftF))
       implicit0(elh: EmbeddableLogHandler[xa.DB]) <- Resource.eval(
                                                       doobieLogging.makeEmbeddableHandler[InitF, RunF, xa.DB](
-                                                        "db-writer-db-logging"
+                                                        "db-writer-logging"
                                                       )
                                                     )
       implicit0(logsDb: Logs[InitF, xa.DB]) = Logs.sync[InitF, xa.DB]
@@ -62,37 +58,6 @@ object App extends EnvApp[AppContext] {
       program = WriterProgram.create[StreamF, RunF](bundle, configs.writer)
       r <- Resource.eval(program.run).mapK(ul.liftF)
     } yield r
-
-  private def makeTxHandler(config: WriterConfig)(
-    implicit
-    bundle: PersistBundle[RunF],
-    consumer: Consumer[_, Option[Tx], StreamF, RunF]
-  ): Resource[InitF, Handler[StreamF]] = Resource.eval {
-    import bundle._
-    for {
-      txn  <- Handle.createOne[Tx, Transaction, InitF, RunF](transaction)
-      in   <- Handle.createNel[Tx, Input, InitF, RunF](input)
-      out  <- Handle.createNel[Tx, Output, InitF, RunF](output)
-      reed <- Handle.createList[Tx, Redeemer, InitF, RunF](redeemer)
-      implicit0(nelHandlers: NonEmptyList[Handle[Tx, RunF]]) = NonEmptyList.of(txn, in, out, reed)
-      handler <- Handler.create[Tx, StreamF, RunF, Chunk, InitF](config)
-    } yield handler
-  }
-
-  private def makeExecutedOrdersHandler(config: WriterConfig)(
-    implicit
-    bundle: PersistBundle[RunF],
-    consumer: Consumer[_, Option[ExecutedOrderEvent], StreamF, RunF]
-  ): Resource[InitF, Handler[StreamF]] = Resource.eval {
-    import bundle._
-    for {
-      deposit <- Handle.createOption[ExecutedOrderEvent, ExecutedDeposit, InitF, RunF](executedDeposit)
-      swap    <- Handle.createOption[ExecutedOrderEvent, ExecutedSwap, InitF, RunF](executedSwap)
-      redeem  <- Handle.createOption[ExecutedOrderEvent, ExecutedRedeem, InitF, RunF](executedRedeem)
-      implicit0(nelHandlers: NonEmptyList[Handle[ExecutedOrderEvent, RunF]]) = NonEmptyList.of(deposit, swap, redeem)
-      handler <- Handler.create[ExecutedOrderEvent, StreamF, RunF, Chunk, InitF](config)
-    } yield handler
-  }
 
   private def makeConsumer[K: RecordDeserializer[RunF, *], V: RecordDeserializer[RunF, *]](
     conf: ConsumerConfig,
