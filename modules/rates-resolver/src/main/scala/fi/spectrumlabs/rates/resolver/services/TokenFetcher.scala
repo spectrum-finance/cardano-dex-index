ce6d0d77e787314912942f3c2994c4541c7644c4
semyonoskin <osckinsemen@yandex.ru>
2022-07-25 21:05:44 +0300

    review comments

--- modules/rates-resolver/src/main/scala/fi/spectrumlabs/rates/resolver/services/TokenFetcher.scala
+++ modules/rates-resolver/src/main/scala/fi/spectrumlabs/rates/resolver/services/TokenFetcher.scala
@@ -3,10 +3,11 @@ package fi.spectrumlabs.rates.resolver.services
 import cats.Monad
 import derevo.circe.magnolia.decoder
 import derevo.derive
+import fi.spectrumlabs.core.models.domain.AssetClass
 import fi.spectrumlabs.core.network.syntax.ResponseOps
 import fi.spectrumlabs.rates.resolver.config.TokenFetcherConfig
 import sttp.client3.circe.asJson
-import sttp.client3.{SttpBackend, UriContext, basicRequest}
+import sttp.client3.{basicRequest, SttpBackend, UriContext}
 import tofu.Throws
 import tofu.higherKind.derived.representableK
 import tofu.syntax.embed._
@@ -14,7 +15,7 @@ import tofu.syntax.monadic._
 
 @derive(representableK)
 trait TokenFetcher[F[_]] {
-  def fetchTokens: F[List[String]]
+  def fetchTokens: F[List[AssetClass]]
 }
 
 object TokenFetcher {
@@ -37,12 +38,12 @@ object TokenFetcher {
     backend: SttpBackend[F, Any]
   ) extends TokenFetcher[F] {
 
-    def fetchTokens: F[List[String]] =
+    def fetchTokens: F[List[AssetClass]] =
       basicRequest
         .get(uri"${conf.url}")
         .response(asJson[TokenResponse])
         .send(backend)
         .absorbError
-        .map(_.tokens.filter(_.network == network).map(_.address))
+        .map(_.tokens.filter(_.network == network).flatMap(tkn => AssetClass.fromString(tkn.address).toList))
   }
 }
